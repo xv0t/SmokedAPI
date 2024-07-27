87d0c4fefd2d8d7c943635dc418fc303a1aefc3b
wheelchair107 <155884667+wheelchair107@users.noreply.github.com>
2024-07-27 11:12:23 +0200

    Smoked globals.cpp

--- src/core/globals.cpp
+++ src/core/globals.cpp
@@ -1,6 +1,6 @@
 namespace globals {
 
-    HMODULE smokeapi_handle = nullptr;
+    HMODULE smokedapi_handle = nullptr;
     HMODULE steamapi_module = nullptr;
     HMODULE vstdlib_module = nullptr;
     HMODULE steamclient_module = nullptr;
