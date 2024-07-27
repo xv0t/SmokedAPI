5afe91a070ef09ef5e93a3a3b99ce910131fc098
wheelchair107 <155884667+wheelchair107@users.noreply.github.com>
2024-07-27 11:14:19 +0200

    Smoked paths.cpp

--- src/core/paths.cpp
+++ src/core/paths.cpp
@@ -6,22 +6,22 @@
 namespace paths {
 
     Path get_self_path() {
-        static const auto path = koalabox::loader::get_module_dir(globals::smokeapi_handle);
+        static const auto path = koalabox::loader::get_module_dir(globals::smokedapi_handle);
         return path;
     }
 
     Path get_config_path() {
-        static const auto path = get_self_path() / "SmokeAPI.config.json";
+        static const auto path = get_self_path() / "SmokedAPI.config.json";
         return path;
     }
 
     Path get_cache_path() {
-        static const auto path = get_self_path() / "SmokeAPI.cache.json";
+        static const auto path = get_self_path() / "SmokedAPI.cache.json";
         return path;
     }
 
     Path get_log_path() {
-        static const auto path = get_self_path() / "SmokeAPI.log.log";
+        static const auto path = get_self_path() / "SmokedAPI.log.log";
         return path;
     }
 
