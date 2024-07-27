294b9f9662963e6961d11de9010d52eb47455911
wheelchair107 <155884667+wheelchair107@users.noreply.github.com>
2024-07-27 11:05:53 +0200

    Smoked main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-#include <smoke_api/smoke_api.hpp>
+#include <smoked_api/smoked_api.hpp>
 
 // This header will be populated at build time
 #include <linker_exports.h>
 
 EXTERN_C [[maybe_unused]] BOOL WINAPI DllMain(HMODULE module_handle, DWORD reason, LPVOID) {
     if (reason == DLL_PROCESS_ATTACH) {
-        smoke_api::init(module_handle);
+        smoked_api::init(module_handle);
     } else if (reason == DLL_PROCESS_DETACH) {
-        smoke_api::shutdown();
+        smoked_api::shutdown();
     }
 
     return TRUE;
